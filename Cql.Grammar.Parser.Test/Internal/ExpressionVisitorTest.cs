bc69fbec891603deab2d579fdd3f0bacba069bf5
Robert Baarda <beardmonkey@hotmail.com>
2017-09-10 15:19:07 +0200

    Added some tests for ExpressionVisitor.

---
+++ Cql.Grammar.Parser.Test/Internal/ExpressionVisitorTest.cs
@@ -0,0 +1,86 @@
+﻿using Cql.Grammar.Parser.Internal;
+using Cql.Query;
+using FluentAssertions;
+using Xunit;
+
+namespace Cql.Grammar.Parser.Test.Internal
+{
+    public class ExpressionVisitorTest
+    {
+        [Fact]
+        public void Test_can_parse_condition_as_expression()
+        {
+            CqlParser cqlParser = CqlParserFactory.CreateParserForQuery("foo != 'bar'");
+            CqlParser.ConditionExpressionContext parseTree =
+                (CqlParser.ConditionExpressionContext)cqlParser.expression();
+
+            ExpressionVisitor visitor = new ExpressionVisitor();
+            ICqlQueryExpression expression = visitor.VisitConditionExpression(parseTree);
+
+            expression.Should().BeOfType<CqlQueryCondition>();
+            expression.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Test_can_parse_parenthesized_expression_containing_condition_expression()
+        {
+            CqlParser cqlParser = CqlParserFactory.CreateParserForQuery("(foo != 'bar')");
+            CqlParser.ParenthesizedExpressionContext parseTree =
+                (CqlParser.ParenthesizedExpressionContext)cqlParser.expression();
+
+            ExpressionVisitor visitor = new ExpressionVisitor();
+            ICqlQueryExpression expression = visitor.VisitParenthesizedExpression(parseTree);
+
+            expression.Should().BeOfType<CqlQueryCondition>();
+            expression.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Test_can_binary_expression_containing_two_condition_expressions()
+        {
+            CqlParser cqlParser = CqlParserFactory.CreateParserForQuery("foo != 'bar' and bar = 'foo'");
+            CqlParser.BinaryExpressionContext parseTree =
+                (CqlParser.BinaryExpressionContext)cqlParser.expression();
+
+            ExpressionVisitor visitor = new ExpressionVisitor();
+            CqlQueryExpression expression = (CqlQueryExpression)visitor.VisitBinaryExpression(parseTree);
+
+            expression.LeftExpression.Should().BeOfType<CqlQueryCondition>();
+            expression.LeftExpression.Should().NotBeNull();
+            expression.RightExpression.Should().BeOfType<CqlQueryCondition>();
+            expression.RightExpression.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Test_can_parse_binary_expression_containing_parenthesized_expression_and_condition_expression()
+        {
+            CqlParser cqlParser = CqlParserFactory.CreateParserForQuery("(foo != 'bar') and bar = 'foo'");
+            CqlParser.BinaryExpressionContext parseTree =
+                (CqlParser.BinaryExpressionContext)cqlParser.expression();
+
+            ExpressionVisitor visitor = new ExpressionVisitor();
+            CqlQueryExpression expression = (CqlQueryExpression)visitor.VisitBinaryExpression(parseTree);
+
+            expression.LeftExpression.Should().BeOfType<CqlQueryCondition>();
+            expression.LeftExpression.Should().NotBeNull();
+            expression.RightExpression.Should().BeOfType<CqlQueryCondition>();
+            expression.RightExpression.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Test_can_parse_parenthesized_expression_for_two_condition_expressions()
+        {
+            CqlParser cqlParser = CqlParserFactory.CreateParserForQuery("(foo != 'bar' and bar = 'foo')");
+            CqlParser.ParenthesizedExpressionContext parseTree =
+                (CqlParser.ParenthesizedExpressionContext)cqlParser.expression();
+
+            ExpressionVisitor visitor = new ExpressionVisitor();
+            CqlQueryExpression expression = (CqlQueryExpression)visitor.VisitParenthesizedExpression(parseTree);
+
+            expression.LeftExpression.Should().BeOfType<CqlQueryCondition>();
+            expression.LeftExpression.Should().NotBeNull();
+            expression.RightExpression.Should().BeOfType<CqlQueryCondition>();
+            expression.RightExpression.Should().NotBeNull();
+        }
+    }
+}
