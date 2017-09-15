cac6554291d2b72dd5882ffd5128377b28740774
Robert Baarda <beardmonkey@hotmail.com>
2017-09-15 17:09:09 +0200

    Added some tests for QueryVisitor.

---
+++ Cql.Grammar.Parser.Test/Internal/QueryVisitorTest.cs
@@ -0,0 +1,96 @@
+﻿using Cql.Grammar.Parser.Internal;
+using Cql.Query;
+using FluentAssertions;
+using Xunit;
+
+namespace Cql.Grammar.Parser.Test.Internal
+{
+    public class QueryVisitorTest
+    {
+        [Theory]
+        [InlineData("select test from start")]
+        [InlineData("select test from root")]
+        [InlineData("select test from 123")]
+        public void Test_can_parse_valid_query_without_where_clause(string query)
+        {
+            CqlParser cqlParser = CqlParserFactory.CreateParserForQuery(query);
+            CqlParser.QueryContext parseTree = cqlParser.query();
+
+            QueryVisitor visitor = new QueryVisitor();
+            CqlQuery cqlQuery = visitor.VisitQuery(parseTree);
+
+            cqlQuery.Should().NotBeNull();
+            cqlQuery.ContentType.Should().NotBeNullOrEmpty();
+            cqlQuery.StartNode.Should().NotBeNull();
+            cqlQuery.Criteria.Should().BeNull();
+        }
+
+        [Theory]
+        [InlineData("select test from start;")]
+        [InlineData("select test from root;")]
+        [InlineData("select test from 123;")]
+        public void Test_can_parse_valid_query_without_where_clause_with_terminator(string query)
+        {
+            CqlParser cqlParser = CqlParserFactory.CreateParserForQuery(query);
+            CqlParser.QueryContext parseTree = cqlParser.query();
+
+            QueryVisitor visitor = new QueryVisitor();
+            CqlQuery cqlQuery = visitor.VisitQuery(parseTree);
+
+            cqlQuery.Should().NotBeNull();
+            cqlQuery.ContentType.Should().NotBeNullOrEmpty();
+            cqlQuery.StartNode.Should().NotBeNull();
+            cqlQuery.Criteria.Should().BeNull();
+        }
+
+        [Theory]
+        [InlineData("select test from start where foo = 'bar'")]
+        [InlineData("select test from root where foo = 'bar'")]
+        [InlineData("select test from 123 where foo = 'bar'")]
+        public void Test_can_parse_valid_query_with_where_clause(string query)
+        {
+            CqlParser cqlParser = CqlParserFactory.CreateParserForQuery(query);
+            CqlParser.QueryContext parseTree = cqlParser.query();
+
+            QueryVisitor visitor = new QueryVisitor();
+            CqlQuery cqlQuery = visitor.VisitQuery(parseTree);
+
+            cqlQuery.Should().NotBeNull();
+            cqlQuery.ContentType.Should().NotBeNullOrEmpty();
+            cqlQuery.StartNode.Should().NotBeNull();
+            cqlQuery.Criteria.Should().NotBeNull();
+        }
+
+        [Theory]
+        [InlineData("select test from start where foo = 'bar';")]
+        [InlineData("select test from root where foo = 'bar';")]
+        [InlineData("select test from 123 where foo = 'bar';")]
+        public void Test_can_parse_valid_query_with_where_clause_with_terminator(string query)
+        {
+            CqlParser cqlParser = CqlParserFactory.CreateParserForQuery(query);
+            CqlParser.QueryContext parseTree = cqlParser.query();
+
+            QueryVisitor visitor = new QueryVisitor();
+            CqlQuery cqlQuery = visitor.VisitQuery(parseTree);
+
+            cqlQuery.Should().NotBeNull();
+            cqlQuery.ContentType.Should().NotBeNullOrEmpty();
+            cqlQuery.StartNode.Should().NotBeNull();
+            cqlQuery.Criteria.Should().NotBeNull();
+        }
+
+        [Theory]
+        [InlineData("selectaaa test from root")]
+        [InlineData("select 123test from 123")]
+        public void Test_cannot_parse_query_with_invalid_select_clause_without_where_clause(string query)
+        {
+            CqlParser cqlParser = CqlParserFactory.CreateParserForQuery(query);
+            CqlParser.QueryContext parseTree = cqlParser.query();
+
+            QueryVisitor visitor = new QueryVisitor();
+            CqlQuery cqlQuery = visitor.VisitQuery(parseTree);
+
+            cqlQuery.Should().BeNull();
+        }
+    }
+}
