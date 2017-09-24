39ad230717b9e24daf10c570f4d4a29210d48dcd
Robert Baarda <beardmonkey@hotmail.com>
2017-09-24 16:05:07 +0200

    Updated project files after renaming parser folders. Added test for cql query service.

---
+++ Cql.Test/FakeCqlQueryRunner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Cql.Query;
+
+namespace Cql.Test
+{
+    public class FakeCqlQueryRunner : ICqlQueryRunner
+    {
+        public CqlQueryExecutionResult ExecuteQueries(IEnumerable<CqlQuery> queries)
+        {
+            return new CqlQueryExecutionResult();
+        }
+    }
+}
