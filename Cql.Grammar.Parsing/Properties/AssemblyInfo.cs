330d99a58793a71db044620dc6427b0d26440038
Robert Baarda <beardmonkey@hotmail.com>
2017-10-01 20:59:52 +0200

    Updated meta and version information in AssemblyInfo files.

--- Cql.Grammar.Parsing/Properties/AssemblyInfo.cs
+++ Cql.Grammar.Parsing/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cql.Grammar.Parser")]
+[assembly: AssemblyProduct("Content Query Language")]
 [assembly: AssemblyCopyright("Copyright © 2017 Robert Baarda")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -37,7 +37,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0-alpha1")]
 
