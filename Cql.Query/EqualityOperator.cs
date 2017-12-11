d3ed8659ac1203921528f8a8d1821bac08ddb22c
Robert Baarda <beardmonkey@hotmail.com>
2017-12-12 00:03:24 +0100

    Forgot to commit equality operator name change for 'Larger Than' to 'Greater Than'.

--- Cql.Query/EqualityOperator.cs
+++ Cql.Query/EqualityOperator.cs
@@ -5,9 +5,9 @@
         None,
         Equals,
         NotEquals,
-        LargerThan,
+        GreaterThan,
         LessThan,
-        LargerThanOrEquals,
+        GreaterThanOrEquals,
         LessThanOrEquals
     }
 }
