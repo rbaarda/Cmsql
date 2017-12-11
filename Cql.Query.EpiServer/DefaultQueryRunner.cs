1e3dde09e96671944826324c410f4fe7cf902d4a
Robert Baarda <beardmonkey@hotmail.com>
2017-12-12 00:04:14 +0100

    Added first version for episerver cql query runner project.

---
+++ Cql.Query.EpiServer/DefaultQueryRunner.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Cql.Query.Execution;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+
+namespace Cql.Query.EpiServer
+{
+    public class DefaultQueryRunner : ICqlQueryRunner
+    {
+        private readonly IPageCriteriaQueryService _pageCriteriaQueryService;
+        private readonly IContentTypeRepository _contentTypeRepository;
+
+        public DefaultQueryRunner(
+            IPageCriteriaQueryService pageCriteriaQueryService,
+            IContentTypeRepository contentTypeRepository)
+        {
+            _pageCriteriaQueryService = pageCriteriaQueryService;
+            _contentTypeRepository = contentTypeRepository;
+        }
+
+        public CqlQueryExecutionResult ExecuteQueries(IEnumerable<CqlQuery> queries)
+        {
+            List<PageData> result = new List<PageData>();
+            foreach (CqlQuery query in queries)
+            {
+                ContentType contentType = _contentTypeRepository.Load(query.ContentType);
+
+                Stack<PropertyCriteriaCollection> propertyCriteriaCollectionStack = new Stack<PropertyCriteriaCollection>();
+                propertyCriteriaCollectionStack.Push(new PropertyCriteriaCollection());
+                ExpressionVisitor visitor = new ExpressionVisitor(contentType, propertyCriteriaCollectionStack);
+                query.Criteria.Accept(visitor);
+
+                PageReference searchStartNodeRef = GetStartSearchFromNode(query.StartNode);
+
+                foreach (PropertyCriteriaCollection propertyCriteriaCollection in propertyCriteriaCollectionStack)
+                {
+                    PageDataCollection foundPages = _pageCriteriaQueryService.FindPagesWithCriteria(
+                        searchStartNodeRef,
+                        propertyCriteriaCollection);
+                    if (foundPages != null && foundPages.Any())
+                    {
+                        result.AddRange(foundPages);
+                    }
+                }
+            }
+
+            return new CqlQueryExecutionResult
+            {
+                QueryResults = result.Select(MapPageDataToCqlQueryResult)
+            };
+        }
+
+        private PageReference GetStartSearchFromNode(CqlQueryStartNode startNode)
+        {
+            switch (startNode.StartNodeType)
+            {
+                case CqlQueryStartNodeType.Start:
+                    return ContentReference.StartPage;
+                case CqlQueryStartNodeType.Root:
+                    return ContentReference.RootPage;
+                case CqlQueryStartNodeType.Id:
+                    if (int.TryParse(startNode.StartNodeId, out int rootNodeId))
+                    {
+                        return new PageReference(rootNodeId);
+                    }
+                    break;
+            }
+            return PageReference.EmptyReference;
+        }
+
+        private CqlQueryResult MapPageDataToCqlQueryResult(PageData page)
+        {
+            return new CqlQueryResult
+            {
+                Name = page.Name
+            };
+        }
+    }
+}
---
+++ Cql.Query.EpiServer/ExpressionVisitor.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cql.Query.Execution;
+using EPiServer;
+using EPiServer.DataAbstraction;
+using EPiServer.Filters;
+
+namespace Cql.Query.EpiServer
+{
+    internal class ExpressionVisitor : ICqlQueryExpressionVisitor
+    {
+        private readonly ContentType _contentType;
+
+        public Stack<PropertyCriteriaCollection> PropertyCriteriaCollectionStack { get; }
+
+        public ExpressionVisitor(
+            ContentType contentType,
+            Stack<PropertyCriteriaCollection> propertyCriteriaCollectionStack)
+        {
+            _contentType = contentType;
+            PropertyCriteriaCollectionStack = propertyCriteriaCollectionStack;
+        }
+
+        public void VisitQueryCondition(CqlQueryCondition condition)
+        {
+            PropertyDefinition propDef = _contentType.PropertyDefinitions
+                .FirstOrDefault(prop =>
+                    prop.Name.Equals(condition.Identifier, StringComparison.InvariantCultureIgnoreCase));
+            if (propDef != null)
+            {
+                CompareCondition compareCondition = MapEqualityOperatorToCompareCondition(condition.Operator);
+                PropertyCriteriaCollectionStack.Peek().Add(new PropertyCriteria
+                {
+                    Condition = compareCondition,
+                    Value = condition.Value,
+                    Name = condition.Identifier,
+                    Type = propDef.Type.DataType,
+                    Required = true
+                });
+            }
+        }
+
+        public void VisitQueryExpression(CqlQueryBinaryExpression binaryExpression)
+        {
+            if (binaryExpression.Operator == ConditionalOperator.Or)
+            {
+                PropertyCriteriaCollectionStack.Push(new PropertyCriteriaCollection());
+            }
+
+            ExpressionVisitor leftExpressionVisitor = new ExpressionVisitor(
+                _contentType,
+                PropertyCriteriaCollectionStack);
+            binaryExpression.LeftExpression.Accept(leftExpressionVisitor);
+
+            if (binaryExpression.Operator == ConditionalOperator.Or)
+            {
+                PropertyCriteriaCollectionStack.Push(new PropertyCriteriaCollection());
+            }
+
+            ExpressionVisitor rightExpressionVisitor = new ExpressionVisitor(
+                _contentType,
+                PropertyCriteriaCollectionStack);
+            binaryExpression.RightExpression.Accept(rightExpressionVisitor);
+        }
+
+        private CompareCondition MapEqualityOperatorToCompareCondition(EqualityOperator operatr)
+        {
+            switch (operatr)
+            {
+                case EqualityOperator.Equals:
+                    return CompareCondition.Equal;
+                case EqualityOperator.GreaterThan:
+                    return CompareCondition.GreaterThan;
+                case EqualityOperator.LessThan:
+                    return CompareCondition.LessThan;
+                case EqualityOperator.NotEquals:
+                    return CompareCondition.NotEqual;
+            }
+
+            throw new InvalidOperationException($"Equality operator '{operatr}' not supported.");
+        }
+    }
+}
