778126e4ec0d1c85919f6814b49f9f1fb876f8e8
Robert Baarda <beardmonkey@hotmail.com>
2017-12-19 21:58:53 +0100

    Created small and simple build script.

--- build.ps1
+++ build.ps1
@@ -1,73 +1,26 @@
 param(
-    [string]$target = "test",
-    [string]$configuration = "Release",
-    [string]$buildAssemblyVersion = "",
-    [string]$buildSemanticVersion = ""
+    [string]$projectName,
+    [string]$buildAssemblyVersion,
+    [string]$buildSemanticVersion
 )
 
 Set-StrictMode -Version 2
 Set-Location $PSScriptRoot
 
-$packageOutputFolder = (join-path (Get-Location) "artifacts\packages")
+(Get-Content $projectName\Properties\AssemblyInfo.cs -Encoding UTF8) -replace "\[assembly: AssemblyVersion\(`".*`"\)\]", "[assembly: AssemblyVersion(`"$($buildAssemblyVersion)`")]" | Set-Content $projectName\Properties\AssemblyInfo.cs -Encoding UTF8
+(Get-Content $projectName\Properties\AssemblyInfo.cs -Encoding UTF8) -replace "\[assembly: AssemblyInformationalVersion\(`".*`"\)\]", "[assembly: AssemblyInformationalVersion(`"$($buildSemanticVersion)`")]" | Set-Content $projectName\Properties\AssemblyInfo.cs -Encoding UTF8
 
-function Exec([string] $command, [string] $message = "") {
-    if ($message -eq "") {
-        $message = $command
-    }
-    Write-Host -ForegroundColor DarkGray ("EXEC: " + $message)
-    Write-Host ""
-    Invoke-Expression $command
-    Write-Host ""
+$packageOutputFolder = ".\artifacts\packages"
 
-    if ($LASTEXITCODE -ne 0) {
-        exit 1
-    }
+& nuget restore
+
+$vswhere = ".\packages\vswhere.2.2.11\tools\vswhere.exe"
+$msbuild = & $vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
+if ($msbuild) {
+  $msbuild = join-path $msbuild "MSBuild\15.0\Bin\MSBuild.exe"
+  if (test-path $msbuild) {
+    & $msbuild $projectName\$projectName`.csproj /t:Rebuild /p:Configuration=Release /v:normal /m /nologo
+  }
 }
 
-function MsBuild(
-	[string] $project,
-	[string] $configuration,
-	[string] $target = "build",
-	[string] $verbosity = "minimal",
-	[string] $message = "",
-	[string] $binlogFile = "") {
-    $cmd = '&"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\bin\msbuild" ' + $project + ' /t:' + $target + ' /p:Configuration=' + $configuration + ' /v:' + $verbosity + ' /m /nologo'
-    if ($binlogFile -ne "") {
-        $cmd = $cmd + " /bl:" + $binlogFile
-    }
-    Exec $cmd $message
-}
-
-function NugetPack {
-    param(
-	    [System.IO.FileInfo[]][Parameter(ValueFromPipeLine=$True)] $nuspecFiles,
-	    [string] $outputFolder,
-	    [string] $configuration)
-    process {
-        $nuspecFiles | ForEach-Object {
-            Exec ('c:\dev\nuget\nuget.exe pack "' + $_.FullName + '" -NonInteractive -NoPackageAnalysis -OutputDirectory "' + $outputFolder + '" -Properties Configuration=' + $configuration)
-        }
-    }
-}
-
-function BuildStep([string] $message) {
-    Write-Host -ForegroundColor White $("==> " + $message + " <==")
-    Write-Host ""
-}
-
-function CreatePackages() {
-    BuildStep "Creating NuGet packages"
-    Get-ChildItem -Recurse -Filter *.nuspec | NugetPack -outputFolder $packageOutputFolder -configuration $configuration
-}
-
-function MakeDir([string] $path) {
-    if ((test-path $path) -eq $false) {
-        New-Item -Type directory -Path $path | out-null
-    }
-}
-
-MakeDir $packageOutputFolder
-
-MsBuild "Cql.sln" $configuration
-
-CreatePackages
+& nuget pack $projectName`.nuspec -NonInteractive -NoPackageAnalysis -OutputDirectory $packageOutputFolder -Properties configuration=Release`;version=$buildSemanticVersion
