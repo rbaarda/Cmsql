537db0b4de4e2f1549bf07bb0657ca11ec45bb33
Robert Baarda <beardmonkey@hotmail.com>
2018-02-24 20:11:43 +0100

    Moved meta data property mappings into PropertyDataTypeResolver class.

--- Cql.EpiServer/Internal/PropertyDataTypeResolver.cs
+++ Cql.EpiServer/Internal/PropertyDataTypeResolver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using EPiServer.Core;
 using EPiServer.DataAbstraction;
@@ -7,6 +8,46 @@ namespace Cql.EpiServer.Internal
 {
     internal class PropertyDataTypeResolver
     {
+        private static readonly IDictionary<string, PropertyDataType>
+            MetaDataPropertyTypeMappings = new Dictionary<string, PropertyDataType>
+            {
+                {MetaDataProperties.PageLink, PropertyDataType.PageReference},
+                {MetaDataProperties.PageTypeID, PropertyDataType.PageType},
+                {MetaDataProperties.PageParentLink, PropertyDataType.PageReference},
+                {MetaDataProperties.PagePendingPublish, PropertyDataType.Boolean},
+                {MetaDataProperties.PageWorkStatus, PropertyDataType.Number},
+                {MetaDataProperties.PageDeleted, PropertyDataType.Boolean},
+                {MetaDataProperties.PageSaved, PropertyDataType.Boolean},
+                {MetaDataProperties.PageTypeName, PropertyDataType.String},
+                {MetaDataProperties.PageChanged, PropertyDataType.Date},
+                {MetaDataProperties.PageCreatedBy, PropertyDataType.String},
+                {MetaDataProperties.PageChangedBy, PropertyDataType.String},
+                {MetaDataProperties.PageDeletedBy, PropertyDataType.String},
+                {MetaDataProperties.PageDeletedDate, PropertyDataType.Date},
+                {MetaDataProperties.PageCreated, PropertyDataType.Date},
+                {MetaDataProperties.PageMasterLanguageBranch, PropertyDataType.String},
+                {MetaDataProperties.PageLanguageBranch, PropertyDataType.String},
+                {MetaDataProperties.PageGUID, PropertyDataType.String},
+                {MetaDataProperties.PageContentAssetsID, PropertyDataType.String},
+                {MetaDataProperties.PageContentOwnerID, PropertyDataType.String},
+                //{MetaDataProperties.PageFolderID, PropertyDataType.Number},
+                {MetaDataProperties.PageVisibleInMenu, PropertyDataType.Boolean},
+                {MetaDataProperties.PageURLSegment, PropertyDataType.String},
+                {MetaDataProperties.PagePeerOrder, PropertyDataType.Number},
+                {MetaDataProperties.PageExternalURL, PropertyDataType.String},
+                {MetaDataProperties.PageChangedOnPublish, PropertyDataType.Boolean},
+                {MetaDataProperties.PageCategory, PropertyDataType.Category},
+                {MetaDataProperties.PageStartPublish, PropertyDataType.Date},
+                {MetaDataProperties.PageStopPublish, PropertyDataType.Date},
+                {MetaDataProperties.PageCreated, PropertyDataType.Date},
+                {MetaDataProperties.PageArchiveLink, PropertyDataType.PageReference},
+                {MetaDataProperties.PageShortcutType, PropertyDataType.Number},
+                {MetaDataProperties.PageShortcutLink, PropertyDataType.PageReference},
+                {MetaDataProperties.PageTargetFrame, PropertyDataType.Number},
+                {MetaDataProperties.PageLinkURL, PropertyDataType.String},
+                {MetaDataProperties.PageName, PropertyDataType.String}
+            };
+
         private readonly ContentType _contentType;
 
         internal PropertyDataTypeResolver(ContentType contentType)
@@ -22,9 +63,9 @@ namespace Cql.EpiServer.Internal
             }
 
             propertyDataType = PropertyDataType.String;
-            if (MetaDataPropertyTypeMapping.Mappings.ContainsKey(propertyIdentifier))
+            if (MetaDataPropertyTypeMappings.ContainsKey(propertyIdentifier))
             {
-                propertyDataType = MetaDataPropertyTypeMapping.Mappings[propertyIdentifier];
+                propertyDataType = MetaDataPropertyTypeMappings[propertyIdentifier];
                 return true;
             }
 
