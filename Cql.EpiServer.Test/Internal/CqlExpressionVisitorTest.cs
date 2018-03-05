14080d2d50fb12f915ccb0a9df02c7ee8cc827fa
Robert Baarda <beardmonkey@hotmail.com>
2018-03-05 21:35:00 +0100

    Added some tests for cql expression visitor.

--- Cql.EpiServer.Test/Internal/CqlExpressionVisitorTest.cs
+++ Cql.EpiServer.Test/Internal/CqlExpressionVisitorTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Cql.EpiServer.Internal;
 using Cql.Query;
 using EPiServer;
@@ -12,7 +13,7 @@ namespace Cql.EpiServer.Test.Internal
     public class CqlExpressionVisitorTest
     {
         [Fact]
-        public void Test_can_parse_query_condition_to_property_criteria()
+        public void Test_can_map_query_condition_to_property_criteria()
         {
             // Arrange
             CqlQueryCondition condition = new CqlQueryCondition
@@ -40,6 +41,53 @@ namespace Cql.EpiServer.Test.Internal
             propertyCriteria.Value.ShouldBeEquivalentTo(condition.Value);
             propertyCriteria.Condition.ShouldBeEquivalentTo(CompareCondition.GreaterThan);
             propertyCriteria.Name.ShouldBeEquivalentTo(condition.Identifier);
+        }
+
+        [Fact]
+        public void Test_when_condition_is_null_criteria_should_be_empty()
+        {
+            // Arrange
+            CqlExpressionVisitorContext context = new CqlExpressionVisitorContext();
+
+            CqlExpressionVisitor cqlExpressionVisitor =
+                new CqlExpressionVisitor(
+                    new QueryConditionToPropertyCriteriaMapper(
+                        new PropertyDataTypeResolver(new ContentType())), context);
+
+            // Act
+            cqlExpressionVisitor.VisitQueryCondition(null);
+
+            IEnumerable<PropertyCriteriaCollection> propertyCriteriaCollection = context.GetCriteria();
+            
+            // Assert
+            propertyCriteriaCollection.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Test_when_condition_is_cannot_be_mapped_criteria_should_be_empty()
+        {
+            // Arrange
+            CqlQueryCondition condition = new CqlQueryCondition
+            {
+                Identifier = "ThisPropertyCannotBeFound",
+                Operator = EqualityOperator.GreaterThan,
+                Value = "5"
+            };
+
+            CqlExpressionVisitorContext context = new CqlExpressionVisitorContext();
+
+            CqlExpressionVisitor cqlExpressionVisitor =
+                new CqlExpressionVisitor(
+                    new QueryConditionToPropertyCriteriaMapper(
+                        new PropertyDataTypeResolver(new ContentType())), context);
+
+            // Act
+            cqlExpressionVisitor.VisitQueryCondition(condition);
+
+            IEnumerable<PropertyCriteriaCollection> propertyCriteriaCollection = context.GetCriteria();
+
+            // Assert
+            propertyCriteriaCollection.Should().BeEmpty();
         }
     }
 }
