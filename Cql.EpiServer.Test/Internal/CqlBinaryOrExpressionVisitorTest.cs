08735217cacc8908d66f6ac3ed8b89d915aec403
Robert Baarda <beardmonkey@hotmail.com>
2018-03-08 21:34:09 +0100

    Added some tests for binary or expression visitor.

--- Cql.EpiServer.Test/Internal/CqlBinaryOrExpressionVisitorTest.cs
+++ Cql.EpiServer.Test/Internal/CqlBinaryOrExpressionVisitorTest.cs
@@ -36,5 +36,79 @@ namespace Cql.EpiServer.Test.Internal
             // Assert
             propertyCriteriaCollection.Should().HaveCount(1);
         }
+
+        [Fact]
+        public void Test_when_orexpression_visitor_visits_andexpression_with_conditions_push_one_new_criteria_collection()
+        {
+            // Arrange
+            CqlQueryBinaryExpression expression = new CqlQueryBinaryExpression
+            {
+                Operator = ConditionalOperator.And,
+                LeftExpression = new CqlQueryCondition
+                {
+                    Identifier = "PageName",
+                    Operator = EqualityOperator.GreaterThan,
+                    Value = "5"
+                },
+                RightExpression = new CqlQueryCondition
+                {
+                    Identifier = "PageName",
+                    Operator = EqualityOperator.GreaterThan,
+                    Value = "5"
+                }
+            };
+
+            CqlExpressionVisitorContext context = new CqlExpressionVisitorContext();
+
+            CqlExpressionVisitor cqlExpressionVisitor =
+                new CqlBinaryOrExpressionVisitor(
+                    new QueryConditionToPropertyCriteriaMapper(
+                        new PropertyDataTypeResolver(new ContentType())), context);
+
+            // Act
+            cqlExpressionVisitor.VisitQueryExpression(expression);
+
+            IEnumerable<PropertyCriteriaCollection> propertyCriteriaCollection = context.GetCriteria();
+
+            // Assert
+            propertyCriteriaCollection.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void Test_when_orexpression_visitor_visits_orexpression_push_criteria_collection_for_every_condition()
+        {
+            // Arrange
+            CqlQueryBinaryExpression expression = new CqlQueryBinaryExpression
+            {
+                Operator = ConditionalOperator.Or,
+                LeftExpression = new CqlQueryCondition
+                {
+                    Identifier = "PageName",
+                    Operator = EqualityOperator.GreaterThan,
+                    Value = "5"
+                },
+                RightExpression = new CqlQueryCondition
+                {
+                    Identifier = "PageName",
+                    Operator = EqualityOperator.GreaterThan,
+                    Value = "5"
+                }
+            };
+
+            CqlExpressionVisitorContext context = new CqlExpressionVisitorContext();
+
+            CqlExpressionVisitor cqlExpressionVisitor =
+                new CqlBinaryOrExpressionVisitor(
+                    new QueryConditionToPropertyCriteriaMapper(
+                        new PropertyDataTypeResolver(new ContentType())), context);
+
+            // Act
+            cqlExpressionVisitor.VisitQueryExpression(expression);
+
+            IEnumerable<PropertyCriteriaCollection> propertyCriteriaCollection = context.GetCriteria();
+
+            // Assert
+            propertyCriteriaCollection.Should().HaveCount(2);
+        }
     }
 }
